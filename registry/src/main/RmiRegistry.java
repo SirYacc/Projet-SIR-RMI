49cbcbbd0fe6feb177fca7720186483d0ca9ceaf
Christophe Guieu <christophe.guieu@gmail.com>
2015-01-07 13:32:52 +0100

    suprresion argument de lancement

--- registry/src/main/RmiRegistry.java
+++ registry/src/main/RmiRegistry.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.rmi.RMISecurityManager;
 import java.rmi.registry.LocateRegistry;
 
 import javax.rmi.ssl.SslRMIClientSocketFactory;
@@ -8,7 +7,16 @@ import javax.rmi.ssl.SslRMIServerSocketFactory;
 
 public class RmiRegistry {
 	public static void main (String[] args) throws Exception{
-		System.setSecurityManager(new RMISecurityManager());
+		
+		System.setProperty("javax.net.ssl.keyStore", "../data/registryKeystore.jks");
+		System.setProperty("javax.net.ssl.keyStorePassword", "ilfaitbeau");
+		
+		System.setProperty("javax.net.ssl.trustStore", "../data/registryTruststore.jks");
+		System.setProperty("javax.net.ssl.trustStorePassword", "cocolasticot");
+		
+		System.setProperty("javax.rmi.ssl.client.enabledProtocols", "TLSv1.2");
+		System.setProperty("javax.rmi.ssl.client.enabledCipherSuites", "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384");
+		
 		LocateRegistry.createRegistry(1099, new SslRMIClientSocketFactory(),
 				new SslRMIServerSocketFactory(
 						new String[] {"TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384"},
--- server/src/main/EchoImpl.java
+++ server/src/main/EchoImpl.java
@@ -28,10 +28,19 @@ public class EchoImpl extends UnicastRemoteObject implements Echo {
 	}
 	
 	public static void main (String args[]) throws Exception {
-		System.setSecurityManager(new RMISecurityManager());
+		
+		System.setProperty("javax.net.ssl.keyStore", "../data/serverKeystore.jks");
+		System.setProperty("javax.net.ssl.keyStorePassword", "ilfaitbeau");
+		
+		System.setProperty("javax.net.ssl.trustStore", "../data/serverTruststore.jks");
+		System.setProperty("javax.net.ssl.trustStorePassword", "cocolasticot");
+		
+		System.setProperty("javax.rmi.ssl.client.enabledProtocols", "TLSv1.2");
+		System.setProperty("javax.rmi.ssl.client.enabledCipherSuites", "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384");
+		
 		Registry registry = LocateRegistry.getRegistry(null, 1099, 
 				new SslRMIClientSocketFactory());
-		EchoImpl obj = new EchoImpl();
+		Echo obj = new EchoImpl();
 		registry.rebind("rmi://localhost/EchoServer", obj);
 		System.out.println("EchoServer bound in registry");
 	}
