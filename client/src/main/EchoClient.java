610dac79d17860982cb9e62d5fb0ec4fc6e2bc45
Christophe Guieu <christophe.guieu@gmail.com>
2015-01-07 13:07:38 +0100

    passage des arguments de configuration dans le code

--- client/src/main/EchoClient.java
+++ client/src/main/EchoClient.java
@@ -7,6 +7,16 @@ import javax.rmi.ssl.SslRMIClientSocketFactory;
 
 public class EchoClient {
 	public static void main(String args[]) throws Exception {
+		
+		System.setProperty("javax.net.ssl.keyStore", "../data/clientKeystore.jks");
+		System.setProperty("javax.net.ssl.keyStorePassword", "ilfaitbeau");
+		
+		System.setProperty("javax.net.ssl.trustStore", "../data/clientTruststore.jks");
+		System.setProperty("javax.net.ssl.trustStorePassword", "cocolasticot");
+		
+		System.setProperty("javax.rmi.ssl.client.enabledProtocols", "TLSv1.2");
+		System.setProperty("javax.rmi.ssl.client.enabledCipherSuites", "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384");
+		
 		System.setSecurityManager(new RMISecurityManager());
 		Registry registry = LocateRegistry.getRegistry(null, 1099, 
 				new SslRMIClientSocketFactory());
